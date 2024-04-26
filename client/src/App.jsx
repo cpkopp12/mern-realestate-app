fb47b2fe8a7e35295f43bc9567eb62818630f19f
cpkopp <cameronpkopp@gmail.com>
2024-04-25 19:46:53 -1000

    header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/Signup';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 // APP FUNCTIONAL COMPONENT -----------------------
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
