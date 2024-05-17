9c28c2ee16955f2dbfed180d517c660453ed92d8
cpkopp <cameronpkopp@gmail.com>
2024-05-16 16:51:31 -1000

    started signin controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -14,3 +14,19 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async (req, res, next) => {
+  // read req.body
+  const { email, password } = req.body;
+
+  try {
+    // validate email
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'User not found.'));
+    // check password
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials.'));
+  } catch (error) {
+    next(error);
+  }
+};
