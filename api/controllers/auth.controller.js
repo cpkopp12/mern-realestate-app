3abeb5c515c848a6b79620ac2280e19a0e3bd04e
cpkopp <cameronpkopp@gmail.com>
2024-05-18 13:05:43 -1000

    signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -26,6 +27,15 @@ export const signin = async (req, res, next) => {
     // check password
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials.'));
+    // generate token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    // destructure to take password out of response
+    const { password: pass, ...rest } = validUser._doc;
+    // create cookie
+    res
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
